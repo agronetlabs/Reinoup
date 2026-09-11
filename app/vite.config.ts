968afe62321aee33f65b59959f35517569aa3622
agronetlabs <leandro@agronet.io>
2026-09-11 14:41:27 -0300

    Updated vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -15,6 +15,10 @@ const base = process.env.BASE_PATH ?? '/';
 // https://vite.dev/config/
 export default defineConfig({
   base,
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
